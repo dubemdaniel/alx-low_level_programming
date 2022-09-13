686e8cfae530299cb40a28f7c48df30f0f8e2c96
dubemdaniel <princedaniel7890@gmail.com>
2022-09-13 06:00:44 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- *  * print_alphabet - Entry point
- *   * Description: a function that prints the alphabet,
- *    * in lowecase followed by a new line
- *     * Return: void
- *      */
+ *  * print_alphabet - print alphabet
+ *   *
+ *    * Return: void
+ *     */
 void print_alphabet(void)
 {
 		char letter = 'a';
